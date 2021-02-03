3656989900dac16773fb6914c2809a146bf92091
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-02-03 15:42:51 -0800

    tests pass

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+export default class Coordinates extends Component {
+
+    handleClick = (e) => {
+        this.coords([e.clientX, e.clientY])
+    }
+    
+    coords = (array) => this.props.onReceiveCoordinates(array)
+
+    render () {
+        console.log(this.props)
+        return (
+            <button onClick={this.handleClick}>Coordinates</button>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react'
+
+export default class DelayedButton extends Component {
+    
+    handleClick = (e) => {
+        e.persist();
+        setTimeout(() => {this.passedFun(e);}, this.delay())
+    };
+
+    passedFun = (e) => this.props.onDelayedClick(e);
+    delay = () => this.props.delay
+
+    render () {
+        return (
+            <button onClick={this.handleClick}>Delay</button>
+        )
+    }
+}
